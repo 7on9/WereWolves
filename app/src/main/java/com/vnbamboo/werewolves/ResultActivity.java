22204aa9b439e615e8c079b6172e881874965a1b
Ống Tre <tamdaulong207@gmail.com>
2018-09-07 01:24:17 +0700

    version 1.0 100% done

---
+++ app/src/main/java/com/vnbamboo/werewolves/ResultActivity.java
@@ -0,0 +1,55 @@
+package com.vnbamboo.werewolves;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( @Nullable Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        getSupportActionBar().hide();
+
+        final byte cardOrder[] = getIntent().getByteArrayExtra("Order list");
+        final TextView txtRoleColumn = (TextView) findViewById(R.id.txtRoleColumnResult);
+        final TextView txtNumColumn = (TextView) findViewById(R.id.txtIdColumnResult);
+
+        final Button btnConfirm = (Button) findViewById(R.id.btnConfirmToSeeResult);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+
+                //link 2 column
+
+                //convert data to string
+                String roleColumn = new String();
+                String numColumn = new String();
+
+                for (int id = 0; id < cardOrder.length; id++) {
+                    roleColumn += MenuActivity.FULL_ROLE_NAME[cardOrder[id]] + "\n";
+                    numColumn += Integer.toString(id + 1) + "\n";
+                }
+                //set compoment
+                txtNumColumn.setText(numColumn);
+                txtRoleColumn.setText(roleColumn);
+                btnConfirm.setText("Trở về menu chính!");
+                btnConfirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick( View v ) {
+                        finish();
+                    }
+                });
+            }
+        });
+    }
+}
--- app/src/main/java/com/vnbamboo/werewolves/ShowRoleActivity.java
+++ app/src/main/java/com/vnbamboo/werewolves/ShowRoleActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ShowRoleActivity extends AppCompatActivity {
 
@@ -22,11 +23,15 @@ public class ShowRoleActivity extends AppCompatActivity {
         final byte cardOrder[] = getIntent().getByteArrayExtra("Order list");
 
         //View thisView = this; // getLayoutInflater().inflate(R.layout.activity_next_player, null);
+        final TextView txtYourRole = (TextView) findViewById(R.id.txtYourRole);
+        txtYourRole.setText("Bấm vào lá bài để xem vai trò của bạn!");
         final ImageView card = (ImageView) findViewById(R.id.imgCard);
+
         card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick( View v ) {
                 card.setImageResource(getResources().getIdentifier("com.vnbamboo.werewolves:drawable/" + MenuActivity.PATH_IMG_ROLE[cardOrder[roleOfThisPlayer]], null, null));
+                txtYourRole.setText( "Bạn là " + MenuActivity.FULL_ROLE_NAME[cardOrder[roleOfThisPlayer]]);
             }
         });
 
@@ -35,7 +40,7 @@ public class ShowRoleActivity extends AppCompatActivity {
             @Override
             public void onClick( View v ) {
                 if(nextRole == cardOrder.length)
-                    finish();
+                    toResult(cardOrder);
                 else
                     nextCard(cardOrder, nextRole);
             }
@@ -45,6 +50,11 @@ public class ShowRoleActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NextPlayerActivity.class);
         intent.putExtra("Order list", cardOrder);
         intent.putExtra("Position", nextRole);
+        super.startActivity(intent);
+    }
+    public void toResult(byte[] cardOrder){
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("Order list", cardOrder);
         super.startActivity(intent);
     }
 }
